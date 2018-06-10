3c80f699afecccb0359cb063d0177861cd886473
Omar Jesus Mejia <omargotico@hotmail.com>
2018-06-10 13:19:11 -0500

    Problema-1 finish but ugly

--- problema-1/cuadrobateo.py
+++ problema-1/cuadrobateo.py
@@ -1,44 +1,48 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+
 # sphinx_gallery_thumbnail_number = 2
-
-vegetables = [x + (3/11) for x in range(0, 11)]
-farmers = [-1.5, -1.0, -0.5, 0, 0.5, 1.0, 1.5]
-
-harvest = np.array([[0.300, 0.320, 0.320, 0.350, 0.330, 0.315, 0.310],
-                    [0.310, 0.340, 0.340, 0.350, 0.340, 0.340, 0.320],
-                    [0.310, 0.340, 0.340, 0.350, 0.340, 0.340, 0.320],
-                    [0.340, 0.380, 0.380, 0.400, 0.390, 0.390, 0.320],
-                    [0.360, 0.390, 0.390, 0.400, 0.390, 0.390, 0.320],
-                    [0.360, 0.390, 0.390, 0.400, 0.380, 0.380, 0.310],
-                    [0.320, 0.340, 0.340, 0.330, 0.300, 0.300, 0.280],
-                    [0.320, 0.340, 0.340, 0.330, 0.275, 0.270, 0.260],
-                    [0.280, 0.300, 0.300, 0.300, 0.260, 0.250, 0.250],
-                    [0.270, 0.290, 0.300, 0.300, 0.250, 0.240, 0.240],
-                    [0.350, 0.270, 0.270, 0.260, 0.240, 0.240, 0.230]])
+y = np.around(np.arange(4, .99, -3/10), 2)
+x = np.arange(-1.5, 2.0, 0.5)
 
 
-fig, ax = plt.subplots()
-im = ax.imshow(harvest)
+def LoadData():
+    try:
+        data = np.genfromtxt('TedWilliams.csv', delimiter=',')
+    except Exception as e:
+        print(e)
+    finally:
+        return data
 
-# We want to show all ticks...
-ax.set_xticks(np.arange(len(farmers)))
-ax.set_yticks(np.arange(len(vegetables)))
 
-# ... and label them with the respective list entries
-ax.set_xticklabels(farmers)
-ax.set_yticklabels(vegetables)
+def setInfo(ax, data):
+    for i in range(len(y)):
+        for j in range(len(x)):
+            text = ax.text(j, i, data[i, j],
+                           ha="center", va="center", color="w")
+    ax.set_title("HeatMap")
 
-# Rotate the tick labels and set their alignment.
-#plt.setp(ax.get_xticklabels(), rotation=45, ha="right",rotation_mode="anchor")
 
-# Loop over data dimensions and create text annotations.
-for i in range(len(vegetables)):
-    for j in range(len(farmers)):
-        text = ax.text(j, i, harvest[i, j],
-                       ha="center", va="center", color="w")
+def main():
+    data = LoadData()
+    fig, ax = plt.subplots(1, 1, figsize=(11, 7))
+    ax.imshow(data)
 
-ax.set_title("Harvest of local farmers (in tons/year)")
-# fig.tight_layout()
-plt.show()
+    ax.set_xticks(np.arange(len(x)))
+    ax.set_yticks(np.arange(len(y)))
+
+    ax.set_xticklabels(x)
+    ax.set_yticklabels(y)
+
+    ax.set_xticks(np.arange(len(x)))
+    ax.set_yticks(np.arange(len(y)))
+
+    ax.set_xticklabels(x)
+    ax.set_yticklabels(y)
+    fig.tight_layout()
+    setInfo(ax, data)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
